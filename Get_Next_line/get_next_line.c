67d3ebfa9ca6dbe4b2ffd888e8c2e9a5a39a9c6e
Jungmin Oh <junoh@c6r3s4.42seoul.kr>
2021-12-20 19:07:33 +0900

    gnl 2021-12-20

--- Get_Next_line/get_next_line.c
+++ Get_Next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: junoh <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/19 17:27:24 by junoh             #+#    #+#             */
-/*   Updated: 2021/12/19 18:21:09 by junoh            ###   ########.fr       */
+/*   Updated: 2021/12/20 19:05:18 by junoh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,44 @@
 char	*get_next_line(int fd)
 {
 	static t_list	*head;
-	char			*buf;
+	char			*buf[BUFFER_SIZE]; // nullstring trminate
 	int				read_size;
 	// add more variables
 	
-	buf = (char *)malloc(BUFFER_SIZE)
-	if (fd < 0 || BUFFER_SIZE < 0 || buf == NULL)
+	if (fd < 0 || BUFFER_SIZE < 0)
 		return (0);
-	while ()
-		
+	read_size = 0;
+	while (read_size % BUFFER_SIZE == 0 || read_size == -1)
+	{
+		read_size = ft_find_and_read(fd, buf, BUFFER_SIZE);
 }
+
+int	ft_find_and_read(int fd, char *buf, int BUFFER_SIZE)
+{
+	int		size;
+	char	*tmp;
+
+	if(read(fd, buf, BUFFER_SIZE) <= 0) // if function find EOF or ERROR 
+		return (-1);
+	if (ft_strichr(s, '\n') + 1 == BUFFER_SIZE)
+		return (
+}
+
+int	ft_strichr(const char *s, char c)
+{
+	int		index;
+
+	index = 0;
+	while (*s)
+	{
+		if (*s == c)
+			return (index);
+		index++;
+		s++;
+	}
+	if (c == 0)
+		return (index);
+}
+
+		
+
